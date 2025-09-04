0e378b9d189f1c6d59ebaafbc037e2e680f81755
DeviBabariya <devibabariyarnw@gmail.com>
2025-09-04 22:42:09 +0530

    modules

---
+++ module.js
@@ -0,0 +1,35 @@
+//Module(Method) JS:
+
+    //Core Modules [in-built by node_js] => http, fs, os, path, buffer, url, event and more...
+
+            // CJS => Common Javascript
+            // ESM => ECMA Script Module
+
+
+    // Third-Party Modules [installed with npm packages]
+
+
+    //Local Modules [user-defined methods]
+
+            // const addition =(a,b)=>{
+            //     return a + b ;
+            // }
+
+            // const product =(a,b)=>{
+            //     return a * b ;
+            // }
+
+            
+            // module.exports = addition;  //default export (es-5)
+
+
+            // exports.square = (n) => n**2; //Named export (es-5)
+
+
+
+            // export default {addition , product}; //default export(es-6)
+
+            // export const square=(n)=> n**2;  //named export (es-6)
+
+
+
--- test.js
+++ test.js
@@ -1,6 +1,29 @@
 
-console.log(__dirname);   //Directory Path
+// console.log(__dirname);   //Directory Path
 
-console.log(__filename);  //File Path
+// console.log(__filename);  //File Path
+
+
+
+// default & named imports es-5(old-method)
+
+        // const hello = require("./module")     //default
+        // console.log(hello(22,11));
+
+
+        // const {square} = require("./module");  //named
+        // console.log(square(7));
+
+
+
+// default & named imports es-6(latest-method)
+
+        // import hello from "./module.js"     // default
+        // console.log(hello.addition(10,20));
+
+        // import { square } from "./module.js";   //named
+        // console.log(square(11));
+
+
 
 
